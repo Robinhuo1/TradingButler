a3931d3cdbed541b9c4f5d8a11672caab3f91635
Robin Huo <huo.robin@gmail.com>
2022-11-01 16:29:00 -0500

    fully closed only

---
+++ summary.py
@@ -0,0 +1,136 @@
+import json
+from dateutil.parser import parse
+from decimal import Decimal, ROUND_HALF_UP
+import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pathlib import Path
+
+base = Path(__file__).parents[0]
+env = Environment(
+    loader=FileSystemLoader(base),
+    autoescape=select_autoescape()
+)
+
+
+def read_json_file(file_name):
+    with open(file_name, 'r') as f:
+        trades = json.load(f, parse_float=Decimal)
+    return reversed(trades)
+
+
+def get_legs(trades):
+    legs = []
+    for trade in trades:
+        instruction = None
+        symbol = None
+        for order_leg in trade['orderLegCollection']:
+            instruction = order_leg['instruction']
+            symbol = order_leg['instrument']['symbol']
+
+        for order_activity in trade['orderActivityCollection']:
+            for execution_leg in order_activity['executionLegs']:
+                legs.append({
+                    'quantity': execution_leg['quantity'],
+                    'price': execution_leg['price'],
+                    'time': execution_leg['time'],
+                    'instruction': instruction,
+                    'symbol': symbol
+                })
+
+    return legs
+
+
+def parse_time(legs):
+    for leg in legs:
+        time = parse(leg['time'])
+        leg['time'] = time
+    return legs
+
+
+def get_positions(legs):
+    positions = []
+    current_positions = {}
+    for leg in legs:
+        if not leg['symbol'] in current_positions:
+            current_positions[leg['symbol']] = []
+            positions.append(current_positions[leg['symbol']])
+        current_positions[leg['symbol']].append(leg)
+        # if leg['quantity'] in current_positions[leg['symbol']][0] != leg['quantity']:
+        #     list = []
+        if leg['instruction'] in ['SELL', 'BUY_TO_COVER']:
+                del current_positions[leg['symbol']]
+    # for position in positions:
+    #     if position[1]['quantity']:
+
+
+
+
+    return positions
+
+
+def get_position_summaries(positions):
+    position_summaries = []
+    for position in positions:
+        position_summary = {}
+        position_summaries.append(position_summary)
+        opening = [l for l in position if l['instruction'] in ['BUY', 'SELL_SHORT']]
+        closing = [l for l in position if l['instruction'] in ['SELL', 'BUY_TO_COVER']]
+        quantity = sum([l['quantity'] for l in opening])
+        risk = sum([l['quantity'] * l['price'] for l in opening])
+        average_price = risk / quantity
+        symbol = opening[0]['symbol']
+        direction = 'Long' if opening[0]['instruction'] == 'BUY' else 'Short'
+        entry_date = position[0]['time'].date()
+        if closing:
+            size = sum([l['quantity'] * l['price'] for l in closing])
+            closing_quantity = sum([l['quantity'] for l in closing])
+            assert closing_quantity == quantity
+            exit_price = size / quantity
+            profit_percentage = ((exit_price / average_price) - 1) * 100
+            profit_percentage = profit_percentage.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+            profit = size - risk
+            exit_date = position[-1]['time'].date()
+            days = (exit_date - entry_date).days
+        else:
+            exit_price = None
+            exit_date = None
+            profit = None
+            profit_percentage = None
+            days = (datetime.datetime.now().date() - entry_date).days
+        position_summary.update({
+            'symbol': symbol,
+            'risk': risk,
+            'entry_date': entry_date,
+            'average_price': average_price,
+            'exit_price': exit_price,
+            'exit_date': exit_date,
+            'days': days,
+            'quantity': quantity,
+            'direction': direction,
+            'profit': profit,
+            'profit_percentage': profit_percentage
+        })
+    return position_summaries
+
+
+def write_output(position_summaries, keys=None, template_file='template.html', output_file='output.html'):
+    if keys is None:
+        keys = ['symbol', 'direction', 'entry_date', 'average_price', 'exit_price', 'exit_date', 'days', 'quantity', 'risk',
+                'profit_percentage', 'profit']
+    template = env.get_template(template_file)
+    output = template.render(position_summaries=position_summaries, keys=keys)
+    with open(output_file, 'w') as f:
+        f.write(output)
+
+
+def main():
+    trades = read_json_file('trades.json')
+    legs = get_legs(trades)
+    legs = parse_time(legs)
+    positions = get_positions(legs)
+    position_summaries = get_position_summaries(positions)
+    write_output(position_summaries)
+
+
+main()
+
---
+++ tests.py
@@ -0,0 +1,159 @@
+import datetime
+from decimal import Decimal, ROUND_HALF_UP
+
+from summary import get_positions, get_position_summaries
+
+fake_data = [{
+    "symbol": "AAPL",
+    "instruction": "BUY",
+    "quantity": Decimal(16.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+    "price": Decimal(29.34).quantize(Decimal('.01'), ROUND_HALF_UP),
+    "time": datetime.datetime(2022, 9, 23)
+    },
+
+    {
+        "symbol": "AAPL",
+        'instruction': 'SELL',
+        'quantity': Decimal(16.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'price': Decimal(222.22).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'time': datetime.datetime(2022, 10, 10)
+    },
+
+    {
+        "symbol": "ATT",
+        'instruction': 'BUY',
+        'quantity': Decimal(6.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'price': Decimal(81.55).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'time': datetime.datetime(2022, 9, 10)
+    },
+
+    {
+        "symbol": "ATT",
+        'instruction': 'SELL',
+        'quantity': Decimal(6.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'price': Decimal(111.25).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'time': datetime.datetime(2022, 9, 20)
+    },
+
+    {
+        "symbol": "AMZN",
+        'instruction': 'BUY',
+        'quantity': Decimal(12.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'price': Decimal(75.25).quantize(Decimal('.01'), ROUND_HALF_UP),
+        'time': datetime.datetime(2022, 10, 20)
+    }
+]
+
+
+def test_get_positions():
+    positions = get_positions(legs=fake_data)
+    assert len(positions) == 3
+    assert positions[0][0]['symbol'] == 'AAPL'
+    assert len(positions[0]) == 2
+    assert positions[0][0]['instruction'] == 'BUY'
+    assert positions[0][0]['price'] == Decimal('29.34')
+    assert positions[0][1]['instruction'] == 'SELL'
+    assert positions[1][0]['symbol'] == 'ATT'
+    assert len(positions[1]) == 2
+    assert positions[1][0]['instruction'] == 'BUY'
+    assert positions[1][0]['price'] == Decimal('81.55')
+    assert positions[1][1]['instruction'] == 'SELL'
+    assert positions[1][0]['quantity'] == Decimal('6.0')
+    assert positions[2][0]['symbol'] == 'AMZN'
+    assert len(positions[2]) == 1
+    assert positions[2][0]['instruction'] == 'BUY'
+    assert positions[2][0]['price'] == Decimal('75.25')
+
+
+def test_get_position_summaries():
+    positions = get_positions(fake_data)
+    position_summaries = get_position_summaries(positions)
+    assert len(position_summaries) == 3
+    assert position_summaries[1]['symbol'] == 'ATT'
+    assert position_summaries[1]['average_price'] == Decimal('81.55')
+    assert position_summaries[1]['risk'] == Decimal('489.300')
+    assert position_summaries[1]['days'] == 10
+    assert position_summaries[1]['profit'] == Decimal('178.2000')
+    assert position_summaries[1]['profit_percentage'] == Decimal('36.42')
+    assert position_summaries[1]['entry_date'] == datetime.date(2022, 9, 10)
+    assert position_summaries[1]['exit_date'] == datetime.date(2022, 9, 20)
+    assert position_summaries[1]['exit_price'] == Decimal('111.25')
+    assert position_summaries[1]['direction'] == 'Long'
+    assert position_summaries[2]['symbol'] == 'AMZN'
+    assert position_summaries[2]['exit_price'] == None
+    assert position_summaries[2]['exit_date'] == None
+    assert position_summaries[2]['profit'] == None
+    assert position_summaries[2]['profit_percentage'] == None
+
+
+def test_partial_closing():
+    partial_closing_trade = [{
+        "symbol": "META",
+        "instruction": "BUY",
+        "quantity": Decimal(10.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        "price": Decimal(241.34).quantize(Decimal('.01'), ROUND_HALF_UP),
+        "time": datetime.datetime(2022, 10, 13)
+         },
+
+        {
+            "symbol": "META",
+            'instruction': 'SELL',
+            'quantity': Decimal(5.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+            'price': Decimal(232.11).quantize(Decimal('.01'), ROUND_HALF_UP),
+            'time': datetime.datetime(2022, 10, 20)
+        },
+        # {
+        #     "symbol": "GE",
+        #     'instruction': 'BUY',
+        #     'quantity': Decimal(18.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        #     'price': Decimal(72.01).quantize(Decimal('.01'), ROUND_HALF_UP),
+        #     'time': datetime.datetime(2022, 9, 20)
+        # },
+        #
+        # {
+        #     "symbol": "GE",
+        #     'instruction': 'SELL',
+        #     'quantity': Decimal(6.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        #     'price': Decimal(54.21).quantize(Decimal('.01'), ROUND_HALF_UP),
+        #     'time': datetime.datetime(2022, 10, 4)
+        # },
+        #
+        # {
+        #     "symbol": "GE",
+        #     'instruction': 'SELL',
+        #     'quantity': Decimal(12.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+        #     'price': Decimal(65.14).quantize(Decimal('.01'), ROUND_HALF_UP),
+        #     'time': datetime.datetime(2022, 10, 17)
+        # }
+    ]
+
+    positions = get_positions(legs=partial_closing_trade)
+    assert len(positions) == 2
+    expected = [
+        [
+            {
+                "symbol": "META",
+                "instruction": "BUY",
+                "quantity": Decimal(5.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+                "price": Decimal(241.34).quantize(Decimal('.01'), ROUND_HALF_UP),
+                "time": datetime.datetime(2022, 10, 13)
+            },
+            {
+                "symbol": "META",
+                "instruction": "SELL",
+                "quantity": Decimal(5.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+                "price": Decimal(241.34).quantize(Decimal('.01'), ROUND_HALF_UP),
+                "time": datetime.datetime(2022, 10, 13)
+            },
+        ],
+        [
+            {
+                "symbol": "META",
+                "instruction": "BUY",
+                "quantity": Decimal(5.0).quantize(Decimal('.01'), ROUND_HALF_UP),
+                "price": Decimal(241.34).quantize(Decimal('.01'), ROUND_HALF_UP),
+                "time": datetime.datetime(2022, 10, 13)
+            }
+        ]
+    ]
+    assert positions == expected
